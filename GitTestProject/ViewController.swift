28f8193d260dcd4bc364402560999e2d7f7c3adb
Mustafa Donmez <mustafadonmez54@outlook.com.tr>
2021-03-21 19:37:28 +0300

    third commit

--- GitTestProject/ViewController.swift
+++ GitTestProject/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("MUSTAFADONMEZ")
         print("Git1")
         print("Git2")
     }
